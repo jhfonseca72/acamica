d4e5c6579c675ebe3449733cb0c48550fed4b0b7
John Fonseca <jhfonseca72@gmail.com>
2021-05-20 18:02:48 -0500

    Download image with js

--- cesde-1/libs/news/main.js
+++ cesde-1/libs/news/main.js
@@ -3,42 +3,42 @@ const API_NEWS_URL = `https://newsapi.org/v2/everything?apiKey=b0fc156ce2d448a38
   * 
   * @param {*} article 
   */
- const createCardNews = (article) => {
-    const news = document.querySelector(".news");
-    const template = document.querySelector("#templateNews");
- 
-    const itemNew = template.content.cloneNode(true);
-    itemNew.querySelector("a").href = article.url;
-    itemNew.querySelector("img").src = article.urlToImage;
-    itemNew.querySelector("h3").textContent = article.title;
-    itemNew.querySelector("p").textContent = article.content;
-    news.appendChild(itemNew);
-  }
+const createCardNews = (article) => {
+  const news = document.querySelector(".news");
+  const template = document.querySelector("#templateNews");
 
- /**
-  * 
-  */
-  const fetchApiNews = async () => {
-    const reqPromise = await fetch(API_NEWS_URL);
-    const news = await reqPromise.json();
-    news.articles.forEach(createCardNews);
-  }
- 
- /**
-  * 
-  */
-  const fetchApiNewsUsingAxios = async () => {
-    const news = await axios.get(API_NEWS_URL);
-    news.articles.forEach(createCardNews);
-  }
+  const itemNew = template.content.cloneNode(true);
+  itemNew.querySelector("a").href = article.url;
+  itemNew.querySelector("img").src = article.urlToImage;
+  itemNew.querySelector("h3").textContent = article.title;
+  itemNew.querySelector("p").textContent = article.content;
+  news.appendChild(itemNew);
+}
 
- /**
-  * Funcion de ejecucion inmediata
-  */
- (async function () {
-    try {
-      await fetchApiNews()
-    } catch(e){
-      console.error(e);
-    }
-  })();
+/**
+ * 
+ */
+const fetchApiNews = async () => {
+  const reqPromise = await fetch(API_NEWS_URL);
+  const news = await reqPromise.json();
+  news.articles.forEach(createCardNews);
+}
+
+/**
+ * 
+ */
+const fetchApiNewsUsingAxios = async () => {
+  const news = await axios.get(API_NEWS_URL);
+  news.articles.forEach(createCardNews);
+}
+
+/**
+ * Funcion de ejecucion inmediata
+ */
+(async function () {
+  try {
+    await fetchApiNews()
+  } catch (e) {
+    console.error(e);
+  }
+})();
